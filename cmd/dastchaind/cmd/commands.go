188038f54c917d02c5e356d217e29dcc86f177cf
Ubuntu <ubuntu@ip-172-31-29-189.eu-central-1.compute.internal>
2023-12-21 00:13:32 +0000

    release

--- cmd/dastchaind/cmd/commands.go
+++ cmd/dastchaind/cmd/commands.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 
 	"cosmossdk.io/log"
@@ -22,6 +24,7 @@ import (
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
 	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
+	typesgen "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
@@ -52,6 +55,7 @@ func initRootCmd(
 		queryCommand(),
 		txCommand(),
 		keys.Commands(),
+		ValidateGenesisCmd(basicManager),
 	)
 }
 
@@ -188,4 +192,51 @@ func appExport(
 	}
 
 	return bApp.ExportAppStateAndValidators(forZeroHeight, jailAllowedAddrs, modulesToExport)
+}
+
+const chainUpgradeGuide = "https://github.com/cosmos/cosmos-sdk/blob/main/UPGRADING.md"
+
+// ValidateGenesisCmd takes a genesis file, and makes sure that it is valid.
+func ValidateGenesisCmd(mbm module.BasicManager) *cobra.Command {
+	return &cobra.Command{
+		Use:     "validate [file]",
+		Aliases: []string{"validate-genesis"},
+		Args:    cobra.RangeArgs(0, 1),
+		Short:   "Validates the genesis file at the default location or at the location passed as an arg",
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			serverCtx := server.GetServerContextFromCmd(cmd)
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			cdc := clientCtx.Codec
+
+			// Load default if passed no args, otherwise load passed file
+			var genesis string
+			if len(args) == 0 {
+				genesis = serverCtx.Config.GenesisFile()
+			} else {
+				genesis = args[0]
+			}
+
+			appGenesis, err := typesgen.AppGenesisFromFile(genesis)
+			if err != nil {
+				return err
+			}
+
+			if err := appGenesis.ValidateAndComplete(); err != nil {
+				return fmt.Errorf("make sure that you have correctly migrated all CometBFT consensus params. Refer the UPGRADING.md (%s): %w", chainUpgradeGuide, err)
+			}
+
+			var genState map[string]json.RawMessage
+			if err = json.Unmarshal(appGenesis.AppState, &genState); err != nil {
+				return fmt.Errorf("error unmarshalling genesis doc %s: %w", genesis, err)
+			}
+
+			if err = mbm.ValidateGenesis(cdc, clientCtx.TxConfig, genState); err != nil {
+				return fmt.Errorf("error validating genesis file %s: %w", genesis, err)
+			}
+
+			fmt.Fprintf(cmd.OutOrStdout(), "File at %s is a valid genesis file\n", genesis)
+			return nil
+		},
+	}
 }
