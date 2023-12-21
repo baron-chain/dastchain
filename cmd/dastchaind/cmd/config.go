81d70c361ad982e787011c1313ea89a71b0e95d9
Ubuntu <ubuntu@ip-172-31-29-189.eu-central-1.compute.internal>
2023-12-21 22:30:52 +0000

    min gas update

--- cmd/dastchaind/cmd/config.go
+++ cmd/dastchaind/cmd/config.go
@@ -44,22 +44,9 @@ func initAppConfig() (string, interface{}) {
 		serverconfig.Config `mapstructure:",squash"`
 	}
 
-	// Optionally allow the chain developer to overwrite the SDK's default
-	// server config.
 	srvCfg := serverconfig.DefaultConfig()
-	// The SDK's default minimum gas price is set to "" (empty value) inside
-	// app.toml. If left empty by validators, the node will halt on startup.
-	// However, the chain developer can set a default app.toml value for their
-	// validators here.
-	//
-	// In summary:
-	// - if you leave srvCfg.MinGasPrices = "", all validators MUST tweak their
-	//   own app.toml config,
-	// - if you set srvCfg.MinGasPrices non-empty, validators CAN tweak their
-	//   own app.toml to override, or use this default value.
-	//
-	// In tests, we set the min gas prices to 0.
-	// srvCfg.MinGasPrices = "0stake"
+	srvCfg.MinGasPrices = "0.001edst"
+	srvCfg.API.Enable = true
 	// srvCfg.BaseConfig.IAVLDisableFastNode = true // disable fastnode by default
 
 	customAppConfig := CustomAppConfig{
